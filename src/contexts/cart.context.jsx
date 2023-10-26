99566d076df352e6ce3db0875cde616ad88048c3
Don <Donmiller150@yahoo.com>
2023-10-26 02:35:47 -0400

    created checkout page and gave ability to add items into cart with images.

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -16,11 +16,31 @@ export const addCartItem = (cartItems, productToAdd) => {
 	return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+	// find the cart item to remove
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === cartItemToRemove.id
+	);
+
+	//check if quantity is equal to 1, if it is remove that item from the cart
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+	}
+
+	// return back cartitems with matching cart item with reduced quantity
+	return cartItems.map((cartItem) =>
+		cartItem.id === cartItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem
+	);
+};
+
 export const CartContext = createContext({
 	isCartOpen: false,
 	setIsOpen: () => {},
 	cartItems: [],
 	addItemToCart: () => {},
+	removeItemFromCart: () => {},
 	cartItemCount: 0,
 });
 
@@ -40,10 +60,14 @@ export const CartProvider = ({ children }) => {
 	const addItemToCart = (product) =>
 		setCartItems(addCartItem(cartItems, product));
 
+	const removeItemToCart = (cartItemToRemove) =>
+		setCartItems(removeCartItem(cartItems, cartItemToRemove));
+
 	const value = {
 		isCartOpen,
 		setIsCartOpen,
 		cartItems,
+		removeItemToCart,
 		addItemToCart,
 		cartItemCount,
 	};
